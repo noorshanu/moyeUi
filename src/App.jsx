0f3de53bad753c523fdc574b064614639856d65a
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-07-29 19:56:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
     <div className='bg-black'>
       <Navbar />
       <Hero />
-      <Marque />
+      {/* <Marque /> */}
       <AboutUs />
       <Features />
       <TokenBenefits />
